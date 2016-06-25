527e0f8d55b935a1fbe90cb6cd9c4412c3ebf0b5
Utkagr <agrawalutkarsh5@gmail.com>
2016-06-25 13:41:20 +0530

    question csv and json updated

--- update_question_features.py
+++ update_question_features.py
@@ -1,12 +1,18 @@
 import json
 import pandas as pd
 pd.set_option('expand_frame_repr', False)
-data = pd.read_csv('Data/map_questions_filters.csv')
+data = pd.read_csv('Data/buy_questions.csv')
 data.index = data['sn']
-def lower_bot_questions():
-	for item in data['bot_question']:
+
+def lower_columns(name):
+	for item in data[str(name)]:
 		item = str(item)
-		item = item.lower()
-		data['bot_question'].index = item
-lower_bot_questions()
-print data['bot_question']
+		item1 = item.lower()
+		for index in data[data[str(name)] == item].index.tolist():
+			data[str(name)][index] = item1
+
+if __name__ == '__main__':
+	column_names = ['bot_question','user_question','key_1','key_2','key_3']
+	for name in column_names:
+		lower_columns(name)
+	print data
