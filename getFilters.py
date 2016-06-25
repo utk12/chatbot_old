4ce2a04eb5d43fbebdb740fc047ed0c9f210d0fc
Maulik <patnimaulik96@gmail.com>
2016-06-25 16:27:31 +0530

    wit-get-filters done

--- getFilters.py
+++ getFilters.py
@@ -2,8 +2,6 @@ from interpret_wit_reply import format_wit_reply
 import json
 
 
-filters_data = getFiltersJson()
-
 def getFiltersJson():
 	with open('Data/filters.json', 'r') as f:
 		data = json.loads(f.read())
@@ -22,24 +20,57 @@ def getFiltersJson():
 				result[cat1][subcat] = ''
 	return result
 
-
+filters_data = getFiltersJson()
 
 def wit_extract_filters(witReply):
 	witReply = format_wit_reply(witReply)
 	filters = {}
+	filters['project_details'] = getProjectDetails(witReply)
 	filters['sports_activities'], filters['club_house'] = getAmenitiesFilters(witReply)
 	filters['specifications'] = getSpecifications(witReply)
 	filters['other'], filters['configurations'] = getConfigurations(witReply)
 	filters['security'] = getSecurityFilters(witReply)
+	print filters
+
+
+def getProjectDetails(witReply):
+	details = {}
+	list1 = ['project_name', 'builder']
+	for i in list1:
+		if i in witReply:
+			details[i] = {}
+			for j in witReply[i]: 
+				details[i][j] = True
+
+	details['address'] = {}
+	if 'loc_city' in witReply:
+		for i in witReply['loc_city']: 
+			details['address']['city'] = i
+			break
+
+	if 'address_zone' in witReply:
+		details['address']['zone'] = {}
+		for i in witReply['address_zone']:
+			details['address']['zone'][i] = True
+
+	if 'specifications' in witReply:
+		if 'parking' in witReply['specifications']:
+			details['car_parking'] = True
+		if 'vastu' in witReply['specifications']:
+			details['vastu_compliant'] = True
+
+	return details
+
 
 
 
 def getSecurityFilters(witReply):
 	filters = {}
-	if ['security'] in witReply:
-		if ['security_place'] in witReply:
+	if 'security' in witReply:
+		if 'security_place' in witReply:
 			filters['place_flag'] = True
 			for i in witReply['security_place']:
+				filters[i] = {}
 				for j in witReply['security']:
 					if j in filters_data['security'][i]:
 						filters[i][j] = True
@@ -53,62 +84,6 @@ def getSecurityFilters(witReply):
 	return filters
 
 
-
-
-def getConfigurations(witReply):
-	others = {}
-	configurations = {}
-	if 'configurations' in witReply:
-		for i in witReply['configurations']:
-			if i in filters_data['other']:
-				others['i'] = True
-			elif i in filters_data['configurations']:
-				configurations[i] = True
-	if 'project_type' in witReply:
-		for i in witReply['project_type']:
-			configurations['property_type'] = witReply['project_type'][i]
-	if 'accommodation' in witReply:
-		for i in witReply['accommodation']:
-			configurations['type'] = witReply['accommodation'][i]
-	if 'area' in witReply:
-		area = []
-		for i in witReply['area']:
-			area.append(i.split()[0])
-		if 'area_type' in witReply:
-			area_type = []
-			for i in witReply['area_type']:
-				area_type.append(i)
-			x = len(area)
-			y = len(area_type)
-			area = [:min(x,y)]
-			area_type = [:min(x,y)]
-			if len(area) == 2:
-				configurations['super_area'] = max(area[0], area[1])
-				configurations['carpet_area'] = min(area[0], area[1])
-			else:
-				configurations[area_type[0]+'_area'] = area[0] 
-		else:
-			configurations['super_area'] = area[0]
-				break
-		if 'level' in witReply:
-			for i in witReply['level']:
-				level.append(i)
-			level = [:len(area)]
-			for i in level:
-				if i in ['high', 'greater']:
-					i = 'high'
-				elif i in['low', 'lower']:
-					i = 'low'
-				else:
-					i = 'medium'
-				configurations['area_level'][i] = True
-		else:
-				configurations['area_level']['medium'] = True
-
-
-
-
-
 def getSpecifications(witReply):
 	filters = {}
 	if 'specifications' in witReply:
@@ -117,7 +92,6 @@ def getSpecifications(witReply):
 				i = 'kitchen_modular'
 			if i in filters_data['specifications']:
 				filters[i] = True
-
 	return filters
 
 
@@ -134,4 +108,62 @@ def getAmenitiesFilters(witReply):
 	return sports, club
 
 
-print json.dumps(getFiltersJson(), indent=4)
+
+def getConfigurations(witReply):
+	others = {}
+	configurations = {}
+	if 'configurations' in witReply:
+		for i in witReply['configurations']:
+			if i in filters_data['other']:
+				others['i'] = True
+			elif i in filters_data['configurations']:
+				configurations[i] = True
+	if 'project_type' in witReply:
+		configurations['property_type'] = {}
+		for i in witReply['project_type']:
+			configurations['property_type'][i] = True
+	if 'accommodation' in witReply:
+		configurations['type']
+		for i in witReply['accommodation']:
+			configurations['type'][i] = True
+	if 'area' in witReply:
+		for i in witReply['area']:
+			configurations['area'] = i
+			break
+		if 'level' in witReply:
+			for i in witReply['level']:
+				if i in ['high', 'greater']:
+					i = 'high'
+				elif i in['low', 'lower']:
+					i = 'low'
+				else:
+					i = 'medium'
+				configurations['area_level'] = i
+				break
+		else:
+				configurations['area_level'] = 'medium'
+
+	if 'amount_of_money' in witReply:
+		for i in witReply['amount_of_money']:
+			configurations['price'] = i
+			break
+		if 'level' in witReply:
+			for i in witReply['level']:
+				if i in ['high', 'greater']:
+					i = 'high'
+				elif i in['low', 'lower']:
+					i = 'low'
+				else:
+					i = 'medium'
+				configurations['price_level'] = i
+				break
+		else:
+			configurations['price_level'] = 'medium'
+
+	return others, configurations
+
+
+
+
+
+# wit_extract_filters({u'security': {u'cctv': True}, u'security_place': {u'tower': True}, u'project_type': {u'row house': True}, u'sentiment': {u'positive': True}, 'loc_city' : {'gurgaon':True}, 'address_zone' : {'sohna road':True}, 'amenities' : {'football':True}})
