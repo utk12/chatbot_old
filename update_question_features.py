40a342b28a1db8af00f487cc9f0d1ea63dbc543d
Utkagr <agrawalutkarsh5@gmail.com>
2016-06-25 17:44:02 +0530

    error ki maa di

--- update_question_features.py
+++ update_question_features.py
@@ -16,7 +16,7 @@ def get_list_distinct_fields(data,key_field):
 	l1 = list(set(data[key_field]))
 	l1 = [l for l in l1 if str(l) != 'nan']
 	return l1
-def update_question_features():
+def update_question_features_key_level():
 	with open('Data/question_features_buy.json') as json_data:
 		feature = json.loads(json_data.read())
 	for key in get_list_distinct_fields(data,'key_2'):
@@ -26,10 +26,7 @@ def update_question_features():
 			#k is projectType,.....
 			k_new = ''.join(map(lambda x: x.lower() if not x.isupper() else "_"+x.lower(),k))
 			if key_new == k_new:
-				print 'yes'
 				for k1 in feature[k]:
-					print k
-					print k1
 					data['bot_question'][data.index[data['key_2'] == key]].item()
 					if k1 == 'bot_question':
 						feature[k][k1] = data['bot_question'][data.index[data['key_2'] == key]].item()
@@ -39,11 +36,31 @@ def update_question_features():
 			# json_data.write(json.dumps(feature,indent = 4,sort_keys = True))
 	print json.dumps(feature,indent = 4)
 
+def update_question_features_value_level():
+	with open('Data/question_features_buy.json') as json_data:
+		feature = json.loads(json_data.read())
+	for key in get_list_distinct_fields(data,'key_3'):
+		# print key
+		key_new = ''.join(map(lambda x:x.lower() if x != ' ' else "_",key))
+		# print key_new
+		for k in feature:
+			for k1 in feature[k]['children']:
+				k1_new = ''.join(map(lambda x: x.lower() if not x.isupper() else "_"+x.lower(),k1))
+				if key_new == k1_new:
+					for k2 in feature[k]['children'][k1]:
+						if k2 == 'bot_question':
+							print data['bot_question'][data.index[data['key_3'] == key]].item()
+							feature[k]['children'][k1][k2] = data['bot_question'][data.index[data['key_3'] == key]].item()
+						elif k2 == 'user_question':
+							print data['user_question'][data.index[data['key_3'] == key]].item()
+							feature[k]['children'][k1][k2] = data['user_question'][data.index[data['key_3'] == key]].item()
+	# print json.dumps(feature,indent = 4)
+
 if __name__ == '__main__':
 	column_names = ['bot_question','user_question','key_1','key_2','key_3']
 	for name in column_names:
 		lower_columns(name)
 
 	# print get_list_distinct_fields(data,'key_2')
-	update_question_features()
-
+	# update_question_features_key_level()
+	update_question_features_value_level()
