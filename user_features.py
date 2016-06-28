77bed98a05362f0fbc805604daba32f3ba546f66
Maulik <patnimaulik96@gmail.com>
2016-06-28 13:27:58 +0530

    Updated user_features.py

--- user_features.py
+++ user_features.py
@@ -31,17 +31,17 @@ def get_feature_dictionary():
 	with open('Data/user_features.json', 'r') as f:
 		data = json.loads(f.read())
 	feature_dict = {}
-	for intent in data:
-		feature_dict[intent] = {}
-		for category in data[intent]:
+	forin data:
+		feature_dict = {}
+		for category in data:
 			category_new = ''.join(map(lambda x: x.lower() if not x.isupper() else "_"+x.lower(), category))
-			feature_dict[intent][category_new] = []
-			for subcategory in data[intent][category]['children']:
+			feature_dict[category_new] = []
+			for subcategory in data[category]['children']:
 				if 'BHK' in subcategory:
 					subcategory = subcategory.lower()
 				else:
 					subcategory = ''.join(map(lambda x: x.lower() if not x.isupper() else "_"+x.lower(), subcategory))
-				feature_dict[intent][category_new].append(subcategory)
+				feature_dict[category_new].append(subcategory)
 	return feature_dict
 
 
@@ -54,29 +54,29 @@ def updateJson(user, userDict):
 	
 
 
-def updateUser(user, intent, features):
+def updateUser(user, features):
 	userDict = getUserDoc(user)
 	for i in features:
-		if i in userDict[intent]:
-			userDict[intent][i]['foundValue'] = 1
-			nfeatures = len(userDict[intent][i]['children'])
-			userDict[intent][i]['preferCount'] = nfeatures/2 + (nfeatures == 1)
-			userDict[intent][i]['factorValue'] += 0.5
+		if i in userDict:
+			userDict[i]['foundValue'] = 1
+			nfeatures = len(userDict[i]['children'])
+			userDict[i]['preferCount'] = nfeatures/2 + (nfeatures == 1)
+			userDict[i]['factorValue'] += 0.5
 		else :
-			for child in userDict[intent]:
-				if i in userDict[intent][child]["children"]:
-					temp = userDict[intent][child]["children"][i] 
-					userDict[intent][child]["children"][i] = 1
-					userDict[intent][child]['foundValue'] = 1
-					userDict[intent][child]['preferCount'] += abs(temp-1)
+			for child in userDict:
+				if i in userDict[child]["children"]:
+					temp = userDict[child]["children"][i] 
+					userDict[child]["children"][i] = 1
+					userDict[child]['foundValue'] = 1
+					userDict[child]['preferCount'] += abs(temp-1)
 					break
 	updateJson(user,userDict)
 
 
 
 
-def getUserVector(user, intent):
-	userDict = getUserDoc(user)[intent]
+def getUserVector(user):
+	userDict = getUserDoc(user)
 	vec = []
 	for category in userDict:
 		a = float(userDict[category]['foundValue'])
@@ -92,8 +92,8 @@ def getUserVector(user, intent):
 		unit_vec = vec
 	return unit_vec
 
-# print getUserVector('uyzpanbd', 'buy')
-# updateUser('uyzpanbd', 'buy', ['security', 'amenities', '2BHK'])
+# print getUserVector('uyzpanbd',
+# updateUser('uyzpanbd', ['security', 'amenities', '2BHK'])
 # print getUserDoc('hndwkoiq')
 # createJSON(getUserId())
 # print get_feature_dictionary()
