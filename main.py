45329603eedb6505c6e6ac7160edb7918dae9d9e
Utkagr <agrawalutkarsh5@gmail.com>
2016-06-28 13:28:01 +0530

    integrate v0.0

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import json
 from sentence_corrector import main
 from wit_get_reply_api import *
+from user_features import *
 
 if __name__ == '__main__':
 	message = 'I want a row-house in golf corse road which should have cricket and football'
 	message =  main(message)
-	output_wit=get_output_wit(message)	#this is whole output from wit
-	entities = get_entities_json_wit(output_wit) #this is json object of entities from wit.
+	entities = get_entities_json_wit(message) #this is json object of entities from wit.
 	dict_features = interpret_wit_output(entities)
 	print json.dumps(entities,indent = 4)
 	print dict_features
+	
+cdhosnhg
